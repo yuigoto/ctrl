4325d3f32fdd7004a975655a166dbdd68536fba9
Fabio Y. Goto <fabio.goto@gmail.com>
2020-01-13 06:20:27 -0300

    Removed 'dist' because I wasn't paying enough attention

--- dist/core/Utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { CtrlProps } from "./Types";
-export declare const CreateEmptyProps: () => CtrlProps;
-export declare const MapDefaultCtrlProps: (props: CtrlProps) => CtrlProps;
-export declare const ValidateCtrlProps: (props: any) => boolean;
-export declare const ToNumericString: (value: string | number) => string;
